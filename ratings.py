2d8d3de2d31b5b80398bd783ec2d63d909265849
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 17:50:14 -0700

    User Adds Restaurant Rating to Dictionary

--- ratings.py
+++ ratings.py
@@ -15,6 +15,30 @@ def create_dictionary(file):
 	for k,v in sorted(restaurant_dict.items()):
 		print("{} is rated at {}.".format(k,v))
 
+	return restaurant_dict
+
+def ask_restaurant():
+	restaurant = input("Please tell us the name of the restaurant you want to rate. ")
+	return restaurant
+
+def ask_rate():
+	rate = input("Please tell us the rating of that restaurant. ")
+	return rate
+
+def add_to_dictionary(restaurant_name,rating,restaurant_dict):
+	restaurant_dict[restaurant_name] = rating
+	sorted(restaurant_dict)
+
+
+	for k,v in sorted(restaurant_dict.items()):
+		print("{} is rated at {}.".format(k,v))
+
+	return restaurant_dict
+
 
 file = open_file("scores.txt")
-create_dictionary(file)
+restaurant_dict = create_dictionary(file)
+
+restaurant_name = ask_restaurant()
+rating = ask_rate()
+add_to_dictionary(restaurant_name, rating, restaurant_dict)
